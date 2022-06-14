bffb6393a14af3a1c02cedd51796bd2f4421be7f
Dmitrii Kurskov <kurskov@users.noreply.github.com>
2022-06-14 16:04:42 +0300

    Added main file template

---
+++ main.ino
@@ -0,0 +1,36 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+#include "FastAtmega328.h"
+#include "SBC55.h"
+
+/* DEBUG MODE */
+//#define DEBUG_ON 
+
+#ifdef DEBUG_ON
+  #define DEBUG(x) Serial.println(x)
+#else
+  #define DEBUG(x)
+#endif
+
+#define STOPERROR for(;;)
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+SBC55 thing();
+
+setup() {
+  #ifdef DEBUG_ON
+    Serial.begin(9600);
+  #endif
+  lcd.begin();
+  lcd.setCursor(0, 0);
+  lcd.print("Loading... ");
+  
+  if (thing.init()) STOPERROR;
+  lcd.print("Ok");
+}
+
+loop() {
+  
+}
