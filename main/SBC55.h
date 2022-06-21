1b7a3f7bdd6d9da5cf9c88d23b65fd3db6e39377
Dmitrii Kurskov <dmitrii@kurskov.ru>
2022-06-21 16:34:13 +0300

    Added setOutput function

--- main/SBC55.h
+++ main/SBC55.h
@@ -25,19 +25,33 @@
   #define DEBUG_SBC(x)
 #endif
 
+// Relay module outputs
+#define OUT_DOORLOCK 1
+#define OUT_VENTVALVE 2
+#define OUT_AIRVALVE 3
+
 class SBC55 {
     public:
-        SBC55() {
+        SBC55(uint8_t latchPin, uint8_t clockPin, uint8_t dataPin) {
             #ifdef DEBUG_SBC_ON
                 Serial.begin(9600);
             #endif
+
+            _latchPin = latchPin;
+            _clockPin = clockPin;
+            _dataPin = dataPin;
+            pinMode(_latchPin, OUTPUT);
+            pinMode(_dataPin, OUTPUT); 
+            pinMode(_clockPin, OUTPUT);
         }
         
         // Check condition and position all parts of machine.
         uint8_t init() {
             DEBUG_SBC("\n> Check condition and position all parts of machine");
             setDoorLockClose();
+            delay(1000);
             setAirValveClose();
+            delay(1000);
             setVentValveOpen();
             delay(1000);
             setDoorLockOpen();
@@ -50,6 +64,7 @@ class SBC55 {
         void afterOn() {
             DEBUG_SBC("\n> After on");
             setVentValveOpen();
+            delay(1000);
             setDoorLockOpen();
             delay(1000);
         }
@@ -59,6 +74,7 @@ class SBC55 {
         void standBy() {
             DEBUG_SBC("\n> Stand by");
             setVentValveOpen();
+            delay(1000);
             setDoorLockOpen();
             delay(1000);
         }
@@ -74,6 +90,7 @@ class SBC55 {
         uint8_t airBlast(uint16_t sec) {
             DEBUG_SBC("\n> Air blast");
             setDoorLockClose();
+            delay(1000);
             setAirValveOpen();
             delay(sec * 1000);
             setAirValveClose();
@@ -88,6 +105,7 @@ class SBC55 {
         uint8_t airBlow(uint16_t sec) {
             DEBUG_SBC("\n> Air blow");
             setDoorLockClose();
+            delay(1000);
             setAirValveOpen();
             delay(sec * 1000);
             setAirValveClose();
@@ -98,16 +116,37 @@ class SBC55 {
         uint8_t drying(uint16_t sec);
 
     private:
+
+        // Arduino pins for connecting to the relay module 
+        // through the 74HC595 cascade.
+        uint8_t _latchPin;  // ST_CP pin of 74HC595
+        uint8_t _clockPin;  // SH_CP pin of 74HC595
+        uint8_t _dataPin;   // DS pin of 74HC595
+        
+        uint16_t _outputs = 0; // outputs state
+
+        // Send outputs state to the relay module.
+        // First - high bit, second - low bit.
+        void setOutputs() {
+            digitalWrite(_latchPin, LOW);
+            shiftOut(_dataPin, _clockPin, MSBFIRST, (_outputs >> 8));
+            shiftOut(_dataPin, _clockPin, MSBFIRST, _outputs);
+            digitalWrite(_latchPin, HIGH);
+        }
         
         /* Safety door lock */
         
         // Open safety door lock
         void setDoorLockOpen() {
+            _outputs &= ~(1<<OUT_DOORLOCK);
+            setOutputs();
             DEBUG_SBC("...Open safety door lock");
         }
         
         // Close safety door lock
         void setDoorLockClose() {
+            _outputs |= (1<<OUT_DOORLOCK);
+            setOutputs();
             DEBUG_SBC("...Close safety door lock");
         }
 
@@ -116,11 +155,15 @@ class SBC55 {
         
         // Open ventilation valve (Y023)
         void setVentValveOpen() {
+            _outputs |= (1<<OUT_VENTVALVE);
+            setOutputs();
             DEBUG_SBC("...Open ventilation valve (Y023 on)");
         }
         
         // Close ventilation valve (Y023)
         void setVentValveClose() {
+            _outputs &= ~(1<<OUT_VENTVALVE);
+            setOutputs();
             DEBUG_SBC("...Close ventilation valve (Y023 off)");
         }
         
@@ -129,11 +172,15 @@ class SBC55 {
         
         // Open air valve (Y024)
         void setAirValveOpen() {
-            DEBUG_SBC("...Open air valve (Y024 on)");
+            _outputs |= (1<<OUT_AIRVALVE);
+            setOutputs();
+            DEBUG_SBC("...Open air valve (Y024 off)");
         }
 
         // Close air valve (Y024)
         void setAirValveClose() {
+            _outputs &= ~(1<<OUT_AIRVALVE);
+            setOutputs();
             DEBUG_SBC("...Close air valve (Y024 off)");
         }
 
@@ -142,6 +189,3 @@ class SBC55 {
 };
 
 #endif // _DK_SBC55_h
-
-// autocreate
-extern SBC55 SBC;
--- main/main.ino
+++ main/main.ino
@@ -14,7 +14,13 @@
 
 #define STOPERROR for(;;)
 
+#define SBC_LATCH_PIN 8     // ST_CP pin of 74HC595
+#define SBC_CLOCK_PIN 12    // SH_CP pin of 74HC595
+#define SBC_DATA_PIN 11     // DS pin of 74HC595
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+SBC55 sbc(SBC_LATCH_PIN, SBC_CLOCK_PIN, SBC_DATA_PIN);
 
 void setup() {
   #ifdef DEBUG_ON
@@ -23,15 +29,23 @@ void setup() {
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("Initialization");
-  lcd.setCursor(0, 1);
-  lcd.print("machine... ");
+  lcd.print("Data load...");
   
-  if (SBC.init()) STOPERROR;
-  SBC.afterOn();
-  lcd.print("Ok");
+  if (sbc.init()) STOPERROR;
+  sbc.afterOn();
+  lcd.print(" Ok");
 }
 
 void loop() {
-  //SBC.airBlast(3);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Air blast");
+  sbc.airBlast(3);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Stand by");
+  sbc.standBy();
+
+  delay(5000);
 }
